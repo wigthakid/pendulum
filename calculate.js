265e0b70fccd0aec6a2566ee53d881ec2fb38f5c
wigthakid <91747483+wigthakid@users.noreply.github.com>
2021-10-29 20:54:53 -0500

    created

---
+++ calculate.js
@@ -0,0 +1,10 @@
+// x_values = []
+// y_values = []
+// for (let i = 0; i < 180; i++) {
+//   r = (3.14 * i)/180
+//   h = 10 
+//   x = h*Math.sin(r)
+//   y = h*Math.cos(r)
+//   x_values.push(x)
+//   y_values.push(y)
+// }
